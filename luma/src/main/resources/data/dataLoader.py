086c2aa16303c989444e4e8155119b1fac19be91
Utkarsh Gupta <utkarshg99@gmail.com>
2022-11-04 20:37:02 +0530

    Static Data created for testing

--- luma/src/main/resources/data/dataLoader.py
+++ luma/src/main/resources/data/dataLoader.py
@@ -10,5 +10,19 @@ def loadEmployee():
         ret = requests.post(BASE_URL+"/add/employee", json= emp, auth= CREDS)
         print(ret.text)
 
+def loadItem():
+    data = json.load(open('Item.json'))
+    for item in data:
+        ret = requests.post(BASE_URL+"/add/item", json= item, auth= CREDS)
+        print(ret.text)
+
+def loadLoan():
+    data = json.load(open('Loan.json'))
+    for loan in data:
+        ret = requests.post(BASE_URL+"/add/loan", json= loan, auth= CREDS)
+        print(ret.text)
+
 if __name__ == "__main__":
     loadEmployee()
+    loadItem()
+    loadLoan()
